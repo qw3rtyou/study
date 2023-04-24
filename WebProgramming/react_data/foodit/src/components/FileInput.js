0754627e1cd4e73ed14dabd3b7f8b4a285cd60ef
qwertyou <chjw1346@gmail.com>
2023-04-24 10:46:13 +0900

    Add showing preview image when upload jpg in react_data

--- WebProgramming/react_data/foodit/src/components/FileInput.js
+++ WebProgramming/react_data/foodit/src/components/FileInput.js
@@ -1,10 +1,42 @@
+import { useRef, useState, useEffect } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+  const [preview, setPreview] = useState();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+    inputNode.value = null;
+    onChange(name, null);
+  };
+
+  useEffect(() => {
+    if (value == null) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(preview);
+    };
+  }, [value]);
+
+  return (
+    <div>
+      <img src={preview} alt="이미지 미리보기" />
+      <input type="file" onChange={handleChange} />
+      <button type="button" onClick={handleClearClick} ref={inputRef}>
+        X
+      </button>
+    </div>
+  );
 }
 
 export default FileInput;
